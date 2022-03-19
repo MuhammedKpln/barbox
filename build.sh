0cec237bf8447f59003d7d246d11dc0e67c6daaa
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-03-19 11:24:28 +0100

    include flutter build command on build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+flutter build macos --release
 rm Spamify.dmg
 {
     appdmg ./config.json ./Spamify.dmg
-} || { 
+} || {
     echo "appdmg is not installed. Please install it and try again."
     exit 1
 }
