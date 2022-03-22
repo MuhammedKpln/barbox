6800ff8bac7d19a4a9891df7684202622c260f59
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-03-22 15:19:20 +0100

    update initial #2

---
+++ lib/types/updates.dart
@@ -0,0 +1,11 @@
+class UpdateModel {
+  UpdateModel({
+    required this.version,
+    required this.changelog,
+    required this.downloadUrl,
+  });
+
+  String version;
+  String changelog;
+  String downloadUrl;
+}
